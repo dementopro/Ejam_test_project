e7f7fb609f637e6d32202ea9314651cfef14aa31
dementopro <dementopro47@gmail.com>
2023-11-08 14:59:03 +0200

    first commit

---
+++ src/pages/ClarifionUpsell.js
@@ -0,0 +1,306 @@
+import styles from "./ClarifionUpsell.module.css";
+
+const ClarifionUpsell = () => {
+  return (
+    <div className={styles.clarifionUpsell}>
+      <div className={styles.usps}>
+        <div className={styles.bg} />
+        <div className={styles.div}>
+          <img
+            className={styles.fluentcheckmarkStarburst20Icon}
+            alt=""
+            src="/fluentcheckmarkstarburst20regular.svg"
+          />
+          <div className={styles.daySatisfactionGuarantee}>
+            30-DAY SATISFACTION GUARANTEE
+          </div>
+        </div>
+        <div className={styles.div1}>
+          <img
+            className={styles.fluentcheckmarkStarburst20Icon}
+            alt=""
+            src="/phtrucklight.svg"
+          />
+          <div className={styles.freeDeliveryOn}>
+            Free delivery on orders over $40.00
+          </div>
+        </div>
+        <div className={styles.div2}>
+          <img
+            className={styles.fluentcheckmarkStarburst20Icon}
+            alt=""
+            src="/mdicardsheartoutline.svg"
+          />
+          <div className={styles.daySatisfactionGuarantee}>
+            50.000+ HAPPY CUSTOMERS
+          </div>
+        </div>
+        <div className={styles.div3}>
+          <img
+            className={styles.fluentcheckmarkStarburst20Icon}
+            alt=""
+            src="/fluentarrowsynccheckmark20regular.svg"
+          />
+          <div className={styles.freeDeliveryOn}>100% Money Back Guarantee</div>
+        </div>
+      </div>
+      <div className={styles.a41c73b670a97ae1d4f47ClarifioParent}>
+        <img
+          className={styles.a41c73b670a97ae1d4f47ClarifioIcon}
+          alt=""
+          src="/600a41c73b670a97ae1d4f47-clarifion-logo-1@2x.png"
+        />
+        <div className={styles.frameParent}>
+          <img
+            className={styles.frameChild}
+            alt=""
+            src="/frame-1484578055.svg"
+          />
+          <img
+            className={styles.nortonAntivirusLogo1Icon}
+            alt=""
+            src="/nortonantiviruslogo-1.svg"
+          />
+        </div>
+      </div>
+      <div className={styles.waitYourOrderInProgressParent}>
+        <div className={styles.waitYour}>Wait ! your order in progress.</div>
+        <div
+          className={styles.loremIpsumDolor}
+        >{`Lorem ipsum dolor sit amet, consectetur adipiscing `}</div>
+      </div>
+      <div className={styles.image4Parent}>
+        <img className={styles.image4Icon} alt="" src="/image-4@2x.png" />
+        <div className={styles.oneTimeOnlySpecialPriceFoParent}>
+          <div className={styles.oneTimeOnlyContainer}>
+            <span>ONE TIME ONLY</span>
+            <span
+              className={styles.specialPriceFor}
+            >{` special price for 6 extra Clarifion for only `}</span>
+            <span>$14 each</span>
+            <span className={styles.specialPriceFor}> ($84.00 total!)</span>
+          </div>
+          <div className={styles.groupParent}>
+            <div className={styles.rectangleParent}>
+              <div className={styles.groupChild} />
+              <img
+                className={styles.imageRemovebgPreview121}
+                alt=""
+                src="/imageremovebgpreview-12-1@2x.png"
+              />
+            </div>
+            <div className={styles.frameGroup}>
+              <div className={styles.clarifionAirIonizerParent}>
+                <div className={styles.clarifionAirIonizer}>
+                  Clarifion Air Ionizer
+                </div>
+                <div className={styles.parent}>
+                  <div className={styles.div4}>$180</div>
+                  <div className={styles.div5}>$84</div>
+                </div>
+              </div>
+              <img className={styles.starsIcon} alt="" src="/stars.svg" />
+              <div className={styles.groupContainer}>
+                <img
+                  className={styles.frameItem}
+                  alt=""
+                  src="/group-1000001340.svg"
+                />
+                <div className={styles.leftInStock}>12 left in Stock</div>
+              </div>
+              <div className={styles.simplyPlugA}>
+                Simply plug a Clarifion into any standard outlet and replace
+                bulky, expensive air purifiers with a simple.
+              </div>
+            </div>
+          </div>
+          <div className={styles.frameContainer}>
+            <div className={styles.tickCircleParent}>
+              <img
+                className={styles.tickCircleIcon}
+                alt=""
+                src="/tickcircle.svg"
+              />
+              <div className={styles.negativeIonTechnologyContainer}>
+                <span>Negative Ion Technology may</span>
+                <b> help with allergens</b>
+              </div>
+            </div>
+            <div className={styles.tickCircleParent}>
+              <img
+                className={styles.tickCircleIcon}
+                alt=""
+                src="/tickcircle.svg"
+              />
+              <div className={styles.designedForAirContainer}>
+                <span>Designed for</span>
+                <b> air rejuvenation</b>
+              </div>
+            </div>
+            <div className={styles.tickCircleParent}>
+              <img
+                className={styles.tickCircleIcon}
+                alt=""
+                src="/tickcircle.svg"
+              />
+              <div className={styles.designedForAirContainer}>
+                <b>Perfect for every room</b>
+                <span> in all types of places.</span>
+              </div>
+            </div>
+          </div>
+          <div className={styles.frameWrapper}>
+            <div className={styles.frameDiv}>
+              <img
+                className={styles.frameInner}
+                alt=""
+                src="/group-1000004656.svg"
+              />
+              <div className={styles.save53AndContainer}>
+                <span>{`Save `}</span>
+                <span className={styles.span}>53%</span>
+                <span>{` and get `}</span>
+                <span className={styles.span}>6 extra Clarifision</span>
+                <span>{` for only `}</span>
+                <span className={styles.span}>$14 Each</span>
+                <span>.</span>
+              </div>
+            </div>
+          </div>
+          <div className={styles.frameParent1}>
+            <div className={styles.frameParent2}>
+              <div className={styles.frameWrapper1}>
+                <div className={styles.yesClaimMyDiscountParent}>
+                  <b className={styles.yesClaim}>Yes - Claim my discount</b>
+                  <img className={styles.lineIcon} alt="" src="/line-1.svg" />
+                </div>
+              </div>
+              <div className={styles.freeShippingParent}>
+                <div className={styles.clarifionAirIonizer}>Free shipping</div>
+                <div className={styles.lineDiv} />
+                <div className={styles.lock71Parent}>
+                  <img className={styles.lock71} alt="" src="/lock-7-1.svg" />
+                  <div className={styles.secure256BitSsl}>
+                    Secure 256-bit SSL encryption.
+                  </div>
+                </div>
+                <div className={styles.lineDiv} />
+                <div className={styles.visaParent}>
+                  <img className={styles.visaIcon} alt="" src="/visa.svg" />
+                  <img className={styles.visaIcon} alt="" src="/shop-pay.svg" />
+                  <img className={styles.visaIcon} alt="" src="/paypal.svg" />
+                  <img
+                    className={styles.visaIcon}
+                    alt=""
+                    src="/mastercard.svg"
+                  />
+                  <img className={styles.visaIcon} alt="" src="/gpay.svg" />
+                  <img
+                    className={styles.visaIcon}
+                    alt=""
+                    src="/apple-pay.svg"
+                  />
+                  <img className={styles.visaIcon} alt="" src="/amex.svg" />
+                </div>
+              </div>
+            </div>
+            <div className={styles.noThanksI}>
+              No thanks, I don’t want this.
+            </div>
+          </div>
+          <div className={styles.image6Parent}>
+            <img className={styles.image6Icon} alt="" src="/image-6@2x.png" />
+            <div className={styles.ifYouAreContainer}>
+              <span>{`If you are not completely thrilled with your Clarifion - We have a `}</span>
+              <b>30 day satisfaction guarantee</b>
+              <span>
+                . Please refer to our return policy at the bottom of the page
+                for more details. Happy Shopping!
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.frameParent3}>
+          <div className={styles.rectangleGroup}>
+            <img
+              className={styles.rectangleIcon}
+              alt=""
+              src="/rectangle-1127@2x.png"
+            />
+            <div className={styles.name}>
+              <img className={styles.starsIcon1} alt="" src="/stars1.svg" />
+              <div className={styles.kenTParent}>
+                <b className={styles.kenT}>Ken T.</b>
+                <div className={styles.verify}>
+                  <div className={styles.verifiedCustomer}>
+                    Verified Customer
+                  </div>
+                  <img
+                    className={styles.verify1Icon}
+                    alt=""
+                    src="/verify-1.svg"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.asSoonAs}>
+            “As soon as the Clarifions arrived I put one in my bedroom. This was
+            late in the afternoon. When I went to the bedroom in the evening it
+            smelled clean. When I went to bed I felt I could breathe better.
+            Wonderful.”
+          </div>
+        </div>
+      </div>
+      <div className={styles.frameParent4}>
+        <div className={styles.copyrightC2023Parent}>
+          <div className={styles.clarifionAirIonizer}>Copyright (c) 2023</div>
+          <div className={styles.frameChild2} />
+          <div className={styles.clarifionsupportclarifioncom}>
+            Clarifionsupport@clarifion.com
+          </div>
+        </div>
+        <div className={styles.lock71Group}>
+          <img className={styles.lock711} alt="" src="/lock-7-11.svg" />
+          <div className={styles.secure256BitSsl}>
+            Secure 256-bit SSL encryption.
+          </div>
+        </div>
+      </div>
+      <div className={styles.frameParent5}>
+        <div className={styles.groupParent1}>
+          <img
+            className={styles.groupIcon}
+            alt=""
+            src="/group-1484578147.svg"
+          />
+          <div className={styles.step1}>Step 1 : Cart Review</div>
+        </div>
+        <div className={styles.groupParent1}>
+          <img
+            className={styles.groupIcon}
+            alt=""
+            src="/group-1484578147.svg"
+          />
+          <div className={styles.step1}>Step 2 : Checkout</div>
+        </div>
+        <div className={styles.groupParent3}>
+          <div className={styles.groupIcon}>
+            <div className={styles.groupItem} />
+            <div className={styles.div6}>3</div>
+          </div>
+          <b className={styles.step3}>Step 3 : Special Offer</b>
+        </div>
+        <div className={styles.groupParent4}>
+          <div className={styles.groupIcon}>
+            <div className={styles.groupInner} />
+            <div className={styles.div6}>4</div>
+          </div>
+          <div className={styles.step3}>Step 4 : Confirmation</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ClarifionUpsell;
